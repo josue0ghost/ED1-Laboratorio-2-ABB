71d746625f1d1cc7fa3a041a2735f2867a876b51
williams <williams@github.com>
2018-02-28 14:35:33 -0600

    Arreglando errores

--- Laboratorio 2 ABB/Laboratorio 2 ABB/Clases/Data.cs
+++ Laboratorio 2 ABB/Laboratorio 2 ABB/Clases/Data.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using EstructurasDeDatosLineales;
+using Laboratorio_2_ABB.Clases;
 using Laboratorio_2_ABB.Models;
 
 namespace Laboratorio_2_ABB.Clases
